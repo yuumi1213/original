360f00f747fcc606039b08b82e5ae7b97cfc411c
EC2 Default User <ec2-user@ip-172-31-45-4.us-east-2.compute.internal>
2020-04-25 11:59:48 +0000

    4/25

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,8 @@
     
     <!--bootstrap-->
     <!--CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <!-- Styles -->
-    <link href="{{ asset('sass/app.scss') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
   </head>
   
   <body>
